6c6eb804fd956c93ea9c90e0703382bbbe6289ca
Staffan Wingren <mail@staffanwingren.se>
2022-12-12 08:53:57 +0100

    Solve day 7 No Space Left on Device.

---
+++ 7-no_space_left_on_device/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadLines(args[0]);
+var root = input.GenerateTree();
+var sizeOfSmallDirs = root.Iter().Where(d => d.Size <= 100000).Sum(d => d.Size);
+Console.WriteLine($"Sum of all dirs smaller than 100000 is {sizeOfSmallDirs}");
+var dirToDelete = root.FindDirToFreeUpSpace();
+Console.WriteLine($"Deleting {dirToDelete.Name} would free up {dirToDelete.Size}");
+
+public static class Helpers
+{
+    public const int DiskSize = 70000000;
+    public const int Requirement = 30000000;
+    public static readonly Regex NavigationCommand = new Regex("\\$ cd (.+)");
+    public static readonly Regex ListCommand = new Regex("\\$ ls");
+    public static readonly Regex FileNode = new Regex("(\\d+) (.+)");
+    public static readonly Regex DirNode = new Regex("dir (.+)");
+    
+    public static DirNode GenerateTree(this IEnumerable<string> input)
+    {
+        var cd = new DirNode("/");
+        var root = cd;
+        foreach (var line in input)
+        {
+            var navigation = NavigationCommand.Match(line);
+            if (navigation.Success)
+            {
+                var nav = navigation.Groups[1];
+                if (nav.Value.Equals(".."))
+                {
+                    cd = cd.Parent;
+                }
+                else if (nav.Value.StartsWith('/'))
+                {
+                    cd = root;
+                }
+                else
+                {
+                    cd = cd.GetChild(nav.Value);
+                }
+            }
+
+            var list = ListCommand.Match(line);
+            if (list.Success)
+            {
+                continue;
+            }
+
+            var file = FileNode.Match(line);
+            if (file.Success)
+            {
+                var size = int.Parse(file.Groups[1].Value);
+                var fileName = file.Groups[2].Value;
+                cd.AddFile(new FileNode(size, fileName, cd));
+            }
+
+            var dir = DirNode.Match(line);
+            if (dir.Success)
+            {
+                var dirName = dir.Groups[1].Value;
+                cd.AddDir(new DirNode(dirName, cd));
+            }
+        }
+
+        return root;
+    }
+
+    public static DirNode FindDirToFreeUpSpace(this DirNode root, int requirement = Requirement, int diskSpace = DiskSize)
+    {
+        var delta = diskSpace - root.Size - requirement;
+        if (int.IsPositive(delta))
+        {
+            return null;
+        }
+
+        return root.Iter().Where(d => d.Size > Math.Abs(delta)).MinBy(d => d.Size);
+    }
+}
+
+public enum NodeType
+{
+    File, 
+    Dir
+}
+
+public interface INode
+{
+    int Size { get; }
+    string Name { get; }
+    NodeType Type { get; }
+    DirNode Parent { get; }
+}
+
+public class DirNode : INode
+{
+    private List<DirNode> _childDirs;
+    private List<FileNode> _childFiles;
+    public int Size => _childDirs.Sum(d => d.Size) + _childFiles.Sum(f => f.Size);
+    
+    public string Name { get; }
+    public NodeType Type => NodeType.Dir;
+    public DirNode Parent { get; }
+    
+    public DirNode(string name, DirNode parent = null)
+    {
+        _childDirs = new List<DirNode>();
+        _childFiles = new List<FileNode>();
+        Name = name;
+        Parent = parent;
+    }
+
+    public DirNode GetChild(string name)
+    {
+        return _childDirs.First(d => d.Name == name);
+    }
+
+    public void AddFile(FileNode fileNode)
+    {
+        _childFiles.Add(fileNode);
+    }
+
+    public void AddDir(DirNode dirNode)
+    {
+        _childDirs.Add(dirNode);
+    }
+
+    public IEnumerable<DirNode> Iter()
+    {
+        yield return this;
+        foreach (var dir in _childDirs)
+        {
+            foreach (var child in dir.Iter())
+            {
+                yield return child;
+            }
+        }
+    }
+}
+
+public class FileNode : INode
+{
+    public int Size { get; }
+    public string Name { get; }
+    public NodeType Type => NodeType.File;
+    public DirNode Parent { get; }
+
+    public FileNode(int size, string name, DirNode parent)
+    {
+        Size = size;
+        Name = name;
+        Parent = parent;
+    }
+}
---
+++ 7-no_space_left_on_device/Tests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+namespace _7_no_space_left_on_device;
+
+[TestFixture]
+public class Tests
+{
+    private readonly string[] Input = new[]
+    {
+        "$ cd /",
+        "$ ls",
+        "dir a",
+        "14848514 b.txt",
+        "8504156 c.dat",
+        "dir d",
+        "$ cd a",
+        "$ ls",
+        "dir e",
+        "29116 f",
+        "2557 g",
+        "62596 h.lst",
+        "$ cd e",
+        "$ ls",
+        "584 i",
+        "$ cd ..",
+        "$ cd ..",
+        "$ cd d",
+        "$ ls",
+        "4060174 j",
+        "8033020 d.log",
+        "5626152 d.ext",
+        "7214296 k ",
+    };
+
+    [Test]
+    public void ShouldCalculateSizeOfRoot()
+    {
+        var tree = Input.GenerateTree();
+        Assert.That(tree.Size, Is.EqualTo(48381165));
+    }
+
+    [Test]
+    public void ShouldCalculateSizeOfA()
+    {
+        var tree = Input.GenerateTree();
+        var a = tree.GetChild("a");
+        Assert.That(a.Size, Is.EqualTo(94853));
+    }
+
+    [Test]
+    public void ShouldCalculateSizeOfD()
+    {
+        var tree = Input.GenerateTree();
+        var d = tree.GetChild("d");
+        Assert.That(d.Size, Is.EqualTo(24933642));
+    }
+
+    [Test]
+    public void ShouldCalculateSumOfDirsLessThan100000()
+    {
+        var dirs = Input.GenerateTree().Iter().Where(d => d.Size <= 100000).Select(d => d.Name);
+        Assert.That(dirs, Contains.Item("a"));
+        Assert.That(dirs, Contains.Item("e"));
+        Assert.That(dirs.Count(), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void ShouldReturnTheSmallestDirNeeded()
+    {
+        var dir = Input.GenerateTree().FindDirToFreeUpSpace();
+        Assert.That(dir.Name, Is.EqualTo("d"));
+        Assert.That(dir.Size, Is.EqualTo(24933642));
+    }
+}
